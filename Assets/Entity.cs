75a7ecabe84edebf2621b0540e31d854c3028086
Xtrife <Alucard.557@gmail.com>
2025-03-22 15:25:13 -0500

    add enemy movement with ground and wall check

--- Assets/Entity.cs
+++ Assets/Entity.cs
@@ -14,37 +14,48 @@ public class Entity : MonoBehaviour
     [Header("Collision info")]
     [SerializeField] protected Transform groundCheck;
     [SerializeField] protected float groundCheckDistance; // Max distance for the Raycast
+    [Space]
+    [SerializeField] protected Transform wallCheck;
+    [SerializeField] protected float wallCheckDistance;
     [SerializeField] protected LayerMask whatIsGround; // specifies which layers should be considered as "ground." Only colliders on these layers will be detected by the raycast
 
     protected bool isGrounded;
+    protected bool isWallDetected;
 
     protected virtual void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
-    
+
+        if (wallCheck == null)
+        {
+            wallCheck = transform;
+        }
     }
 
     protected virtual void Update()
     {
         CollisionChecks();
+
     }
 
     protected virtual void CollisionChecks()
     {
         // Raycast creates an invisible line and, using colliders, returns true if it hits something, and false if it doesn't.
         isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isWallDetected = Physics2D.Raycast(wallCheck.position, Vector2.right, wallCheckDistance * facingDir, whatIsGround);
     }
-    
+
     protected virtual void Flip()
     {
         facingDir = facingDir *= -1;
         facingRight = !facingRight;
         transform.Rotate(0, 180, 0);
     }
-    
+
     protected virtual void OnDrawGizmos()
     {
-        Gizmos.DrawLine(transform.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector2(wallCheck.position.x + wallCheckDistance * facingDir, wallCheck.position.y));
     }
 }
